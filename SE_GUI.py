4762ea8d788d57d31c54fcb73d94c39536c45de2
Kody Ryant <ryantkody@gmail.com>
2025-01-27 13:41:04 -0500

    introduced output window and trying to get it to work while adding more comments to follow procedures.

--- SE_GUI.py
+++ SE_GUI.py
@@ -3,57 +3,72 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QPushButton, QMessageBox, QDialog, QLabel, QLineEdit, QDialogButtonBox, QComboBox
 )
 
-
+#first window you see
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        # Window properties
+        #Window properties
         self.setWindowTitle('Garden Management App')
         self.resize(600, 600)
 
-        # Layout
+        #Layout
         layout = QVBoxLayout()
 
-        # Buttons
+        #Buttons
         btn_input = QPushButton('Input Measurements for New Garden', self)
-        btn_input.clicked.connect(self.open_indoor_outdoor_dialog)
+        btn_input.clicked.connect(self.open_indoor_outdoor_dialog)  #method call
 
         btn_update = QPushButton('Update Existing Garden', self)
-        btn_update.clicked.connect(self.update_garden)
+        btn_update.clicked.connect(self.update_garden)              #method call
 
-        # Add buttons to layout
+        #Add buttons to layout
         layout.addWidget(btn_input)
         layout.addWidget(btn_update)
 
-        # Set layout for the main window
+        #Set layout for the main window
         self.setLayout(layout)
 
+    #make new garden section
     def open_indoor_outdoor_dialog(self):
-        # Open Indoor/Outdoor selection dialog
+        #Open Indoor/Outdoor selection dialog
         dialog = IndoorOutdoorDialog(self)
-        if dialog.exec_() == QDialog.Accepted:
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #error detection needed
+        if dialog.exec_() == QDialog.Accepted:          
             environment = dialog.get_selection()
-            self.open_garden_dimensions_dialog(environment)
-
+            self.open_garden_dimensions_dialog(environment)     #method call
+            
+    #need a seperate call to get text selection
     def open_garden_dimensions_dialog(self, environment):
         # Open garden dimensions input dialog
-        dialog = InputDialog(self)
+        dialog = garden_dimensions_dialog(self)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #error detection needed
         if dialog.exec_() == QDialog.Accepted:
             garden_size = dialog.input_field.text()
-            QMessageBox.information(
-                self,
-                'Garden Input',
-                f'Environment: {environment}\nMeasurements: {garden_size}'
-            )
-
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            #variables carry over and work
+            #need output display now so new class above this
+            #QMessageBox.information(
+               # self,
+               # 'Garden Input',
+               # f'Environment: {environment}\nMeasurements: {garden_size}'
+            #)
+#???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+            #in construction
+            self.output_display = output_window(self.environment, self.garden_size)
+            output_display.show()
+    #update exsisting garden section
     def update_garden(self):
         # Action for "Update Existing Garden" button
-        QMessageBox.information(self, 'Update Garden', 'Feature: Update Existing Garden!')
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #update later, now is just a warning box
+        QMessageBox.information(self, 'Update Garden', 'Update Me!')      
 
-
+#first pop up 
 class IndoorOutdoorDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -86,35 +101,64 @@ class IndoorOutdoorDialog(QDialog):
         # Return selected environment
         return self.combo_box.currentText()
 
-
-class InputDialog(QDialog):
+#second pop up window
+class garden_dimensions_dialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        # Dialog properties
+        #Dialog window properties
         self.setWindowTitle("Input Garden Measurements")
         self.setGeometry(300, 300, 300, 150)
 
-        # Layout
+        #Layout
         layout = QVBoxLayout(self)
 
-        # Label
+        #Label --> text on screen pop up
         layout.addWidget(QLabel("Enter garden measurements (e.g., 10x20x30):", self))
 
-        # Input Field
+        #Input Field --> what they are inputing 
         self.input_field = QLineEdit(self)
         layout.addWidget(self.input_field)
 
-        # Dialog Buttons
+        #Dialog Buttons
         buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
         layout.addWidget(buttons)
 
         self.setLayout(layout)
+        
+#Third pop up window
+#output
+class output_window(QWidget):
+
+    def __init__(self, enviroment, garden_size):
+        super().__init__()
+
+        #Window properties
+        self.setWindowTitle('Garden Management App')
+        self.resize(600, 600)
+
+        #Layout
+        layout = QVBoxLayout()
+
+        #Buttons
+        btn_input = QPushButton('Input Measurements for New Garden', self)
+        btn_input.clicked.connect(self.open_indoor_outdoor_dialog)  #method call
+
+        btn_update = QPushButton('Update Existing Garden', self)
+        btn_update.clicked.connect(self.update_garden)              #method call
+
+        #Add buttons to layout
+        layout.addWidget(btn_input)
+        layout.addWidget(btn_update)
+
+        #Set layout for the main window
+        self.setLayout(layout)
 
 
-# Main Block
+#Main
+#lauch MyApp method to start GUI procedure 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
