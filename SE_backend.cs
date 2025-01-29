9460a0fe95e749a7094b5a3ed46059e49abfb2c6
Kody Ryant <ryantkody@gmail.com>
2025-01-29 12:30:29 -0500

    adding files

---
+++ SE_backend.cs
@@ -0,0 +1,13 @@
+// C# Hello World Program
+using System;
+
+class HelloWorldProgram {
+
+    // Main Method
+    static void Main(string[] args)
+    {
+
+        // "printing Hello World"
+        Console.WriteLine("Hello, World!");
+    }
+}
