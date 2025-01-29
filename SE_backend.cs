726f796c581d0f542638a280496b3c09f96c564b
Kody Ryant <ryantkody@gmail.com>
2025-01-29 15:31:38 -0500

    c# file made, python --> c# communication, not back, xml in progress to communicate with c#. Great work guys.

--- SE_backend.cs
+++ SE_backend.cs
@@ -1,13 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
 // C# Hello World Program
 using System;
 
-class HelloWorldProgram {
+class Backend {
 
     // Main Method
     static void Main(string[] args)
     {
 
         // "printing Hello World"
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+
+        if(args.Length < 2 || args.Length > 2){
+            Console.WriteLine("You didnt provide the nessecary information");
+            return;
+        }
+        
+
+        string environment = args[0];
+        string garden_size = args[1];
+        Console.WriteLine(environment);
+        Console.WriteLine(garden_size);
+
+        Console.WriteLine("got it" + " " + environment + " , " + garden_size);
+        //return;//("here {environment}, {garden_size}");
+        //return("got it {environment}, {garden_size}");
+        
     }
 }
